223b692f843e2cb02c322276ce069a984628a888
Nietaktowny <87383673+Nietaktowny@users.noreply.github.com>
2023-07-22 19:09:51 +0200

    Removed auto running CMock generating script.

--- scripts/cmock_auto_generate.bat
+++ scripts/cmock_auto_generate.bat
@@ -1,15 +1,15 @@
 @echo off
 echo ============================================================= Generating CMock files =============================================================
 
-set "lcd_controller_source=cmock\lib\mocks\Mocklcd_controller.h"
-set "lcd_controller_path=lib\lcd_controller"
+set "lcd_controller_source=C:\Users\User\cmock\lib\mocks\Mocklcd_controller.h"
+set "lcd_controller_path=C:\Users\User\Documents\PlatformIO\Projects\esp32_oscilloscope\lib\lcd_controller"
 
 title Generate CMock files
 echo Starting generating...
 cd C:\Users\User\cmock\lib
-ruby cmock.rb lib\lcd_controller\include\lcd_controller.h
-xcopy /s/z/v/y cmock\lib\mocks\Mocklcd_controller.h lib\lcd_controller\include
-xcopy /s/z/v/y cmock\lib\mocks\Mocklcd_controller.c lib\lcd_controller\src
+ruby cmock.rb C:\Users\User\Documents\PlatformIO\Projects\esp32_oscilloscope\lib\lcd_controller\include\lcd_controller.h
+xcopy /s/z/v/y C:\Users\User\cmock\lib\mocks\Mocklcd_controller.h C:\Users\User\Documents\PlatformIO\Projects\esp32_oscilloscope\lib\lcd_controller\include
+xcopy /s/z/v/y C:\Users\User\cmock\lib\mocks\Mocklcd_controller.c C:\Users\User\Documents\PlatformIO\Projects\esp32_oscilloscope\lib\lcd_controller\src
 echo files stored in library: %lcd_controller_path%
 
 echo ============================================================= CMock files created =============================================================

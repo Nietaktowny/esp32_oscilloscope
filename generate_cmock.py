ccdf7489768cc02f26fe1e121f18dd2b507317e9
Nietaktowny <87383673+Nietaktowny@users.noreply.github.com>
2023-07-22 19:00:05 +0200

    Changed generating paths to relative.

--- generate_cmock.py
+++ generate_cmock.py
@@ -4,11 +4,11 @@ import platform
 if platform.system() == "Linux":
     print("Checked platform: Linux")
     import subprocess
-    subprocess.call([r'C:\Users\User\Documents\PlatformIO\Projects\esp32_oscilloscope\scripts\cmock_auto_generate.bash'])
+    subprocess.call([r'scripts/cmock_auto_generate.bash'])
 elif platform.system() == "Windows":
     print("Checked platform: Windows")
     import subprocess
-    subprocess.call([r'C:\Users\User\Documents\PlatformIO\Projects\esp32_oscilloscope\scripts\cmock_auto_generate.bat'])
+    subprocess.call([r'scripts\cmock_auto_generate.bat'])
 else:
     print("Mac")
 

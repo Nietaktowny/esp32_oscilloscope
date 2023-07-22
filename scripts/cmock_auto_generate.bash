5ba44e6eed5497b09954d785513f28cbc807a93f
Nietaktowny <87383673+Nietaktowny@users.noreply.github.com>
2023-07-22 22:44:04 +0200

    Auto generate CMock files.

--- scripts/cmock_auto_generate.bash
+++ scripts/cmock_auto_generate.bash
@@ -5,9 +5,12 @@ lcd_controller_source="cmock/lib/mocks/Mocklcd_controller.h"
 lcd_controller_path="lib/lcd_controller"
 
 echo Starting generating...
+pwd
 cd cmock/lib
+pwd
 ruby cmock.rb lib/lcd_controller/include/lcd_controller.h
 cd ../..
+pwd
 cp -v cmock/lib/mocks/Mocklcd_controller.h lib/lcd_controller/include
 cp -v cmock/lib/mocks/Mocklcd_controller.c lib/lcd_controller/src
 echo files stored in library: $lcd_controller_path
--- scripts/cmock_auto_generate.bat
+++ scripts/cmock_auto_generate.bat
@@ -6,9 +6,12 @@ set "lcd_controller_path=lib\lcd_controller"
 
 title Generate CMock files
 echo Starting generating...
+cd
 cd cmock\lib
+cd
 ruby cmock.rb lib\lcd_controller\include\lcd_controller.h
 cd ..\..
+cd
 xcopy /s/z/v/y cmock\lib\mocks\Mocklcd_controller.h lib\lcd_controller\include
 xcopy /s/z/v/y cmock\lib\mocks\Mocklcd_controller.c lib\lcd_controller\src
 echo files stored in library: %lcd_controller_path%

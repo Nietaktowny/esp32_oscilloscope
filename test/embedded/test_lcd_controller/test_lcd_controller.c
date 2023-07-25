c62dd565a22d806566d058986b9ee618b30a9bdd
Nietaktowny <87383673+Nietaktowny@users.noreply.github.com>
2023-07-25 08:27:39 +0200

    Added lcd_controller tests.

--- test/embedded/test_lcd_controller/test_lcd_controller.c
+++ test/embedded/test_lcd_controller/test_lcd_controller.c
@@ -1,7 +1,6 @@
 #include "unity.h"
-#include "lcd_controller.h"
 #include "esp_system.h"
-#include "Mocklcd_controller.h"
+#include "lcd_controller.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include <stdio.h>
@@ -14,7 +13,38 @@ void tearDown(void) {
   // clean stuff up here
 }
 
-void test_function_should_not_already_init_spi_bus (void) {
+void test_function_should_test_size_of_lcd_init_cmd_t (void) {
+  //given
+  int size = sizeof(lcd_init_cmd_t);
+  int struct_size = 18;
+
+  //then
+  UnityAssertEqualNumber(struct_size, size, "WRONG INIT STRUCT SIZE", __LINE__, UNITY_DISPLAY_STYLE_INT32);
+}
+
+
+void test_function_should_check_gpio_defines(void) {
+  //given
+  const int RST = 18;
+  const int SCL = 19;
+  const int DC = 21;
+  const int CS = 22;
+  const int SDA = 23;
+  const int SDO = 25;
+  const int BACKLIGHT = 5;
+  //when
+
+  //then
+  UNITY_TEST_ASSERT_EQUAL_INT(RST, PIN_NUM_RST, __LINE__, "Wrong reset GPIO");
+  UNITY_TEST_ASSERT_EQUAL_INT(SCL, PIN_NUM_CLK, __LINE__, "Wrong clock GPIO");
+  UNITY_TEST_ASSERT_EQUAL_INT(DC, PIN_NUM_DC, __LINE__, "Wrong dc GPIO");
+  UNITY_TEST_ASSERT_EQUAL_INT(CS, PIN_NUM_CS, __LINE__, "Wrong chip select GPIO");
+  UNITY_TEST_ASSERT_EQUAL_INT(SDA, PIN_NUM_MOSI, __LINE__, "Wrong mosi GPIO");
+  UNITY_TEST_ASSERT_EQUAL_INT(SDO, PIN_NUM_MISO, __LINE__, "Wrong miso GPIO");
+  UNITY_TEST_ASSERT_EQUAL_INT(BACKLIGHT, PIN_NUM_BCKL, __LINE__, "Wrong backlight GPIO");
+}
+
+void test_function_should_check_that_spi_bus_is_already_init (void) {
   //given
   spi_bus_config_t buscfg={
     .miso_io_num=PIN_NUM_MISO,
@@ -31,7 +61,7 @@ void test_function_should_not_already_init_spi_bus (void) {
 
 
   //then
-  UnityAssertEqualNumber(259, err, "INIT READY SPI BUS WITHOUT CHECK", __LINE__, UNITY_DISPLAY_STYLE_INT32);
+  UnityAssertEqualNumber(259, err, "SPI BUS ALREADY INIT", __LINE__, UNITY_DISPLAY_STYLE_INT32);
 }
 
 void test_function_should_add_spi_device (void) {
@@ -56,8 +86,9 @@ void test_function_should_add_spi_device (void) {
 
 void test_function_should_check_if_init_lcd_returns_ESP_OK(void) {
   //given
+  esp_err_t err;
   //when
-  esp_err_t err = init_lcd();
+  err = init_lcd();
   //then
   UnityAssertEqualNumber(0, err, "INIT LCD RETURNS OK", __LINE__, UNITY_DISPLAY_STYLE_INT16);
 }
@@ -67,8 +98,10 @@ void test_function_should_check_if_init_lcd_returns_ESP_OK(void) {
 int runUnityTests(void) {
   UNITY_BEGIN();
   RUN_TEST(test_function_should_check_if_init_lcd_returns_ESP_OK);
-  RUN_TEST(test_function_should_not_already_init_spi_bus);
+  RUN_TEST(test_function_should_check_that_spi_bus_is_already_init);
   RUN_TEST(test_function_should_add_spi_device);
+  RUN_TEST(test_function_should_test_size_of_lcd_init_cmd_t);
+  RUN_TEST(test_function_should_check_gpio_defines);
   return UNITY_END();
 }
 

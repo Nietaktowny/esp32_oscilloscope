ea7bce41fd89a18d27fea06f2f55ae9b2ff9bd00
Nietaktowny <87383673+Nietaktowny@users.noreply.github.com>
2023-07-25 08:19:56 +0200

    Changed generating mock files to from yml config.

--- scripts/cmock_auto_generate.bash
+++ scripts/cmock_auto_generate.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo ============================================================= Generating CMock files =============================================================
 
-lcd_controller_source="cmock/lib/mocks/Mocklcd_controller.h"
+lcd_controller_source="cmock/lib/mocks/mock_lcd_controller.h"
 lcd_controller_path="lib/lcd_controller"
 
 echo Starting generating...
@@ -11,8 +11,8 @@ pwd
 ruby cmock.rb ../../lib/lcd_controller/include/lcd_controller.h
 cd ../..
 pwd
-cp -v cmock/lib/mocks/Mocklcd_controller.h lib/lcd_controller/include
-cp -v cmock/lib/mocks/Mocklcd_controller.c lib/lcd_controller/src
+cp -v cmock/lib/mocks/mock_lcd_controller.h lib/lcd_controller/include
+cp -v cmock/lib/mocks/mock_lcd_controller.c lib/lcd_controller/src
 echo files stored in library: $lcd_controller_path
 
 echo ============================================================= CMock files created =============================================================

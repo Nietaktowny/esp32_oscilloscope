064edc33dea42aaf0c8b5a516679f982fb9079e2
Nietaktowny <87383673+Nietaktowny@users.noreply.github.com>
2023-07-29 08:25:20 +0200

    Change of mocks structure for native.

--- scripts/cmock_auto_generate.bash
+++ scripts/cmock_auto_generate.bash
@@ -2,17 +2,16 @@
 echo ============================================================= Generating CMock files =============================================================
 
 lcd_controller_source="cmock/lib/mocks/mock_lcd_controller.h"
-lcd_controller_path="lib/lcd_controller"
+mocks_path="test/mocks"
 
 echo Starting generating...
 pwd
 cd cmock/lib
 pwd
-ruby cmock.rb -o../../scripts/CMockConfig.yml ../../lib/lcd_controller/include/lcd_controller.h
+ruby cmock.rb -o../../scripts/CMockConfig.yml ../../test/mocks/lcd_controller.h
+ruby cmock.rb -o../../scripts/CMockConfig.yml ../../test/mocks/spi_driver_mocks.h
 cd ../..
 pwd
-cp -v cmock/lib/mocks/mock_lcd_controller.h lib/lcd_controller/include
-cp -v cmock/lib/mocks/mock_lcd_controller.c lib/lcd_controller/src
-echo files stored in library: $lcd_controller_path
+echo files stored in library: $mocks_path
 
 echo ============================================================= CMock files created =============================================================
